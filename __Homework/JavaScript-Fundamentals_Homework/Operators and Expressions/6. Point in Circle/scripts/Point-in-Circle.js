8e7da2f3a5d217315464bc77177a04e74d3a5de1
madmarks <bratmiro@gmail.com>
2015-05-31 18:58:29 +0300

    Align text inputs to right. Some little bugs resolved.

--- __Homework/JavaScript-Fundamentals_Homework/Operators and Expressions/6. Point in Circle/scripts/Point-in-Circle.js
+++ __Homework/JavaScript-Fundamentals_Homework/Operators and Expressions/6. Point in Circle/scripts/Point-in-Circle.js
@@ -1,6 +1,7 @@
 ﻿// Problem 6. Point in Circle
 //
-// Write an expression that checks if given point P(x, y) is within a circle K({0,0}, 5). //{0,0} is the centre and 5 is the radius
+// Write an expression that checks if given point P(x, y) is within a circle K({0,0}, 5). 
+// {0,0} is the centre and 5 is the radius
 
 function isInsideCircle() {
 
