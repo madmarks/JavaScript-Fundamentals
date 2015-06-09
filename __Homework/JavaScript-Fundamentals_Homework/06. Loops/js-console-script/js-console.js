3e3caf0576f6802432efe6946bb4766aa64db936
madmarks <bratmiro@gmail.com>
2015-06-10 02:00:39 +0300

    write spaces to console added

--- __Homework/JavaScript-Fundamentals_Homework/06. Loops/js-console-script/js-console.js
+++ __Homework/JavaScript-Fundamentals_Homework/06. Loops/js-console-script/js-console.js
@@ -1,71 +1,87 @@
 ﻿(function () {
-	function createJsConsole(selector) {
-		var self = this;
-		//var consoleElement = document.querySelector(selector);
-		var consoleElement = document.getElementById(selector);
+    function createJsConsole(selector) {
+        var self = this;
+        //var consoleElement = document.querySelector(selector);
+        var consoleElement = document.getElementById(selector);
 
-		if (consoleElement.className) {
-			consoleElement.className = consoleElement.className + " js-console";
-		} else {
-			consoleElement.className = "js-console";
-		}
+        if (consoleElement.className) {
+            consoleElement.className = consoleElement.className + " js-console";
+        } else {
+            consoleElement.className = "js-console";
+        }
 
-		var textArea = document.createElement("p");
-		consoleElement.appendChild(textArea);
+        var textArea = document.createElement("p");
+        consoleElement.appendChild(textArea);
 
-		self.write = function jsConsoleWrite(text) {
-			if(text !== undefined && text !== ""){
-				var textLine = document.createElement("span");
-				textLine.innerHTML = text;
-				textArea.appendChild(textLine);
-				consoleElement.scrollTop = consoleElement.scrollHeight;
-			}
-		};
+        self.write = function jsConsoleWrite(text) {
+            if (text !== undefined && text !== "") {
+                var textLine = document.createElement("span");
+                textLine.innerHTML = text;
+                textArea.appendChild(textLine);
+                consoleElement.scrollTop = consoleElement.scrollHeight;
+            }
+        };
 
-		self.writeLine = function jsConsoleWriteLine(text) {
-			self.write(text);
-			textArea.appendChild(document.createElement("br"));
-		};
+        ////////////////////////////////////////////////////////////////////////
+        self.writeSpaces = function jsConsoleWrite(number) {
+            if (number !== undefined && number > 0) {
+                var strSpaces = '',
+                    textLine = document.createElement("span");
+                while (!!number) {
+                    strSpaces = strSpaces.concat('&nbsp;');
+                    number -= 1;
+                }
+                textLine.innerHTML = strSpaces;
+                textArea.appendChild(textLine);
+                consoleElement.scrollTop = consoleElement.scrollHeight;
+            }
+        };
+        ////////////////////////////////////////////////////////////////////////
 
-		self.read = function readText(inputSelector) {
-			var element = document.querySelector(inputSelector);
-			if (element.innerHTML) {
-				return element.innerHTML;
-			} else {
-				return element.value;
-			}
-		};
+        self.writeLine = function jsConsoleWriteLine(text) {
+            self.write(text);
+            textArea.appendChild(document.createElement("br"));
+        };
 
-		self.readInteger = function readInteger(inputSelector) {
-			var text = self.read(inputSelector);
-			return parseInt(text);
-		};
+        self.read = function readText(inputSelector) {
+            var element = document.querySelector(inputSelector);
+            if (element.innerHTML) {
+                return element.innerHTML;
+            } else {
+                return element.value;
+            }
+        };
 
-		self.readFloat = function readFloat(inputSelector) {
-			var text = self.read(inputSelector);
-			return parseFloat(text);
-		};
+        self.readInteger = function readInteger(inputSelector) {
+            var text = self.read(inputSelector);
+            return parseInt(text);
+        };
 
-		self.readArray = function readArray(inputSelector,separator) {
-			var text = self.read(inputSelector);
-			var array = text.split(separator);
-			return array;
-		};
+        self.readFloat = function readFloat(inputSelector) {
+            var text = self.read(inputSelector);
+            return parseFloat(text);
+        };
 
-		self.readNumbersArray = function readNumbersArray(inputSelector, separator) {
-			var array = self.readArray(inputSelector, separator);
-			var numbers = new Array(array.length);
-			for (var i = 0; i < array.length; i++) {
-				numbers[i] = parseFloat(array[i]);
-			}
-			return numbers;
-		};
+        self.readArray = function readArray(inputSelector, separator) {
+            var text = self.read(inputSelector);
+            var array = text.split(separator);
+            return array;
+        };
 
-		self.clearConsole = function clearConsole() {
-		    textArea.innerHTML = "";
-		};
+        self.readNumbersArray = function readNumbersArray(inputSelector, separator) {
+            var array = self.readArray(inputSelector, separator);
+            var numbers = new Array(array.length);
+            for (var i = 0; i < array.length; i++) {
+                numbers[i] = parseFloat(array[i]);
+            }
+            return numbers;
+        };
 
-		return self;
-	}
-	jsConsole = new createJsConsole("js-console");
+        self.clearConsole = function clearConsole() {
+            textArea.innerHTML = "";
+        };
+
+        return self;
+    }
+    jsConsole = new createJsConsole("js-console");
 }).call(this);
