c1e14a22e6941a5b8f7956b21d8e5e3d77d94439
madmarks <bratmiro@gmail.com>
2015-06-10 22:04:46 +0300

    12. Regular Expressions (12.2 doesn't work as expected !)

---
+++ __Homework/JavaScript-Fundamentals_Homework/12. Regular Expressions/1. Format with placeholders/scripts/Format with placeholders.js
@@ -0,0 +1,71 @@
+﻿// Problem 1. Format with placeholders
+//
+// * Write a function that formats a string. The function should have placeholders, as shown in the example
+//     * Add the function to the **String.prototype**
+//
+// 
+// _Example:_
+//
+//  ________________________________________________________________________________________________________________
+// |                               input                                |                   output                  |
+// |--------------------------------------------------------------------|-------------------------------------------|
+// |  var options = {name: 'John'};                                     |                                           |
+// |                                                                    |  'Hello, there! Are you John'             |
+// |  'Hello, there! Are you #{name}?'.format(options);                 |                                           |
+// |--------------------------------------------------------------------|-------------------------------------------|
+// |  var options = {name: 'John', age: 13};                            |                                           |
+// |                                                                    |  'My name is John and I am 13-years-old'  |
+// |  'My name is #{name} and I am #{age}-years-old'.format(options);  |                                           |
+// |----------------------------------------------------------------------------------------------------------------|
+
+
+function printFormattedString() {
+
+    var options;
+
+    String.prototype.format = function (options) {
+
+        // (x)  -> Matches 'x' and remembers the match.
+        //
+        // (?:x) -> Matches 'x' but does not remember the match.
+
+        // When you create a regular expression you have the option of capturing portions of the match and saving them as placeholders ($1, $2, ... $9)
+        //
+        // num = "11222333";
+        //
+        // This regex captures the last 3 digits as capture group $2 and all preceding digits as capture group $1
+        // re = /(\d+)(\d{3})/;
+        //
+
+        // $0 is the entire matched string (#{name} , #{age}),
+        // $1 is the first remembered, because of (\w+), capture group (name , age)
+
+        //return this.replace(/(?:#{(\w+)})/g, function ($0, $1) {
+        return this.replace(/#{(\w+)}/g, function ($0, $1) {
+
+            return options[$1];
+        });
+    }
+    
+    jsConsole.writeLine('var options = {name: \'John\'};');
+    jsConsole.writeLine('');
+
+    jsConsole.writeLine('\'Hello, there! Are you #{name}?\'.format(options);');
+    jsConsole.writeLine('');
+    jsConsole.write('output : ');
+    options = { name: 'John' };
+    jsConsole.writeLine('Hello, there! Are you #{name}?'.format(options));
+    
+    jsConsole.writeLine('---');
+
+    jsConsole.writeLine('var options = {name: \'John\', age: 13}; ');
+    jsConsole.writeLine('');
+
+    jsConsole.writeLine('\'My name is #{name} and I am #{age}-years-old\'.format(options);');
+    jsConsole.writeLine('');
+    jsConsole.write('output : ');
+    options = { name: 'John', age: 13 };
+    jsConsole.writeLine('My name is #{name} and I am #{age}-years-old'.format(options));
+  
+    jsConsole.writeLine('------------------------------------------------------------');
+}
---
+++ __Homework/JavaScript-Fundamentals_Homework/12. Regular Expressions/2. HTML binding/scripts/HTML binding.js
@@ -0,0 +1,82 @@
+﻿// Problem 2. HTML binding
+//
+// *  Write a function that puts the value of an object into the content/attributes of HTML tags. 
+//     * Add the function to the String.prototype
+//
+// 
+// Example 1 :
+//  ____________________________________________________________________________________________________
+// |                        input                        |                    output                    |
+// |-----------------------------------------------------|----------------------------------------------|
+// |  var str = '<div data-bind-content="name"></div>';  |                                              |
+// |                                                     |  <div data-bind-content="name">Steven</div>  |
+// |  str.bind(str, {name: 'Steven'});                   |                                              |
+// |-----------------------------------------------------|----------------------------------------------|
+//
+// 
+// Example 2 :
+//  _____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+// |                                                 input                                                 |                                                               output                                                                |
+// |-------------------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------|
+// |  var bindingString = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>'  |                                                                                                                                     |
+// |                                                                                                       |  <a data-bind-content="name" data-bind-href="link" data-bind-class="name" href="http://telerikacademy.com" class="Elena">Elena</а>  |
+// |  str.bind(str, {name: 'Elena', link: 'http://telerikacademy.com'});                                   |                                                                                                                                     |
+// |-----------------------------------------------------|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+function htmlBinding() {
+
+    var str = '<div data-bind-content="name"></div>',
+        bindingString = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>';
+
+    String.prototype.bind = function (str, params) {
+        var prop,
+            regexHref,
+            regexClass,
+            regexContent,
+            result = this;
+
+        for (prop in params) {
+            regexHref = new RegExp('(<.*?data-bind-href="' + prop + '".*?)>', 'g');
+            regexClass = new RegExp('(<.*?data-bind-class="(' + prop + ')".*?)>', 'g');
+            regexContent = new RegExp('(<.*?data-bind-content="' + prop + '".*?>)(.*?)(<\\s*/.*?>)', 'g');
+
+            result = result.replace(regexContent, function (element, openingTag, content, closingTag) {
+                return openingTag + params[prop] + closingTag;
+            })
+            .replace(regexHref, function (tag, content) {
+                return content + ' href="' + params[prop] + '">';
+            })
+            .replace(regexClass, function (tag, content) {
+                return content + ' class="' + params[prop] + '">';
+            });
+        }
+
+        return result;
+    }
+
+
+    jsConsole.writeLine('var str = \'&lt;div data-bind-content="name"&gt;&lt;/div&gt;\';');
+    jsConsole.writeLine('');
+
+    jsConsole.writeLine('str.bind(str, { name: \'Steven\' });');
+    jsConsole.writeLine('');
+    jsConsole.write('output : ');
+
+    jsConsole.writeLine(str.bind(str, { name: 'Steven' }).replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+
+    jsConsole.writeLine('---');
+
+    //
+
+    jsConsole.writeLine('var bindingString = \'&lt;a data-bind-content="name" data-bind-href="link" data-bind-class="name"&gt;&lt;/а&gt;\'');
+    jsConsole.writeLine('');
+
+    jsConsole.writeLine('str.bind(str, { name: \'Elena\', link: \'http://telerikacademy.com\' });');
+    jsConsole.writeLine('');
+    jsConsole.write('output : ');
+
+    jsConsole.writeLine(str.bind(str, { name: 'Elena', link: 'http://telerikacademy.com' }).replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+
+    jsConsole.writeLine('------------------------------------------------------------');
+}
